ee86c3ceeec8a315cc14781f8dd5ebd8aa78e6d1
KristynaCharova <78442699+KristynaCharova@users.noreply.github.com>
2021-05-21 21:49:34 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import Globalstyle from "./styles/GlobalStyle";
+import GlobalStyle from "./styles/GlobalStyle";
 
 ReactDOM.render(
   <React.StrictMode>
