8f3be03d467440c695eaca25a2718605ab924666
KristynaCharova <78442699+KristynaCharova@users.noreply.github.com>
2021-05-22 18:14:42 +0200

    Added About Page Title

--- src/Pages/AboutPage.js
+++ src/Pages/AboutPage.js
@@ -1,7 +1,20 @@
 import React from "react";
+import styled from "styled-components";
+import Title from "../Components/Title";
+import { MainLayout } from "../styles/Layouts";
 
 function AboutPage() {
-  return <div>About</div>;
+  return (
+    <MainLayout>
+      <AboutStyled>
+        <Title title={"About me"} span={"About me"} />
+      </AboutStyled>
+    </MainLayout>
+  );
 }
+
+const AboutStyled = styled.section`
+  color: red;
+`;
 
 export default AboutPage;
