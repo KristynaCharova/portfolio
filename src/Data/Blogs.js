59e3727f3840c48d9f0a36f67dea6fc051939533
KristynaCharova <78442699+KristynaCharova@users.noreply.github.com>
2021-05-23 11:31:52 +0200

    Added Blog Page

---
+++ src/Data/Blogs.js
@@ -0,0 +1,41 @@
+import blog1 from "../img/blogs/blog1.svg";
+import blog2 from "../img/blogs/blog2.svg";
+import blog3 from "../img/blogs/blog3.svg";
+import blog4 from "../img/blogs/blog4.svg";
+
+const blogs = [
+  {
+    id: 1,
+    title: "How to Work from Home",
+    date: "23",
+    month: "May",
+    image: blog1,
+    link: "http://google.com",
+  },
+  {
+    id: 2,
+    title: "How to use SEO Efficiently Part 1",
+    date: "23",
+    month: "May",
+    image: blog2,
+    link: "http://google.com",
+  },
+  {
+    id: 3,
+    title: "How to choose a programing Language",
+    date: "23",
+    month: "May",
+    image: blog3,
+    link: "http://google.com",
+  },
+  {
+    id: 4,
+    title: "How to use SEO Efficiently Part 2",
+    date: "23",
+    month: "May",
+    image: blog4,
+    link: "http://google.com",
+  },
+];
+
+export default blogs;
--- src/Pages/BlogsPage.js
+++ src/Pages/BlogsPage.js
@@ -1,7 +1,72 @@
 import React from "react";
+import styled from "styled-components";
+import Title from "../Components/Title";
+import { MainLayout, InnerLayout } from "../styles/Layouts";
+
+import blogs from "../Data/Blogs";
 
 function BlogsPage() {
-  return <div>Blogs</div>;
+  return (
+    <MainLayout>
+      <BlogsStyled>
+        <Title title={"Blogs"} span={"Blogs"} />
+        <InnerLayout className="blog">
+          {blogs.map((blog) => {
+            return (
+              <div key={blog.id} className="blog-item">
+                <div className="image">
+                  <img src={blog.image} alt="" />
+                </div>
+                <div className="title">
+                  <a href={blog.link}>{blog.title}</a>
+                </div>
+              </div>
+            );
+          })}
+        </InnerLayout>
+      </BlogsStyled>
+    </MainLayout>
+  );
 }
+
+const BlogsStyled = styled.div`
+  .blog {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-column-gap: 2rem;
+    grid-row-gap: 3rem;
+    .blog-item {
+      padding: 1rem 1rem;
+      background-color: var(--background-dark-grey);
+    }
+    .image {
+      width: 100%;
+      overflow: hidden;
+      padding-bottom: 0.5rem;
+      img {
+        width: 100%;
+        height: 90%;
+        object-fit: cover;
+        transition: all 0.4s ease-in-out;
+        &:hover {
+          cursor: pointer;
+          transform: rotate(3deg) scale(1.1);
+        }
+      }
+    }
+    .title {
+      a {
+        font-size: 1.3rem;
+        padding: 2rem 0;
+        color: var(--white-color);
+        cursor: pointer;
+        transition: all 0.4s ease-in-out;
+        &:hover {
+          color: var(--primary-color);
+        }
+      }
+    }
+  }
+`;
 
 export default BlogsPage;
